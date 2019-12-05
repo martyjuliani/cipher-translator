d96b1b5f33be9779822b6399813812771395ab38
Martin Blazek <marty.blazek@gmail.com>
2019-12-05 01:06:56 +0100

    new maven project and test spring boot application

---
+++ src/main/java/com/juleq/ciphertranslator/CipherTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.juleq.ciphertranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipherTranslatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CipherTranslatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juleq/ciphertranslator/CipherTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juleq.ciphertranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipherTranslatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
